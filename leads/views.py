2a809b5c0c3f8796b12ead41cc07c17e80f5584f
Maciej <nikon.maciej@gmail.com>
2021-04-23 10:47:36 +0200

    Added mixins to class views to check if user is logged in + added signal to auto create UserProfile when User is created

--- leads/views.py
+++ leads/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.mail import send_mail
 from django.http import HttpResponse
 from django.views import generic
@@ -17,19 +18,19 @@ class LandingPageView(generic.TemplateView):
   template_name = "landing.html"
 
 
-class LeadListView(generic.ListView):
+class LeadListView(LoginRequiredMixin, generic.ListView):
   template_name = "leads/lead_list.html"
   queryset = Lead.objects.all()
   context_object_name = "leads"
 
 
-class LeadDetailView(generic.DetailView):
+class LeadDetailView(LoginRequiredMixin, generic.DetailView):
   template_name = "leads/lead_detail.html"
   queryset = Lead.objects.all()
   context_object_name = "lead"
 
 
-class LeadCreateView(generic.CreateView):
+class LeadCreateView(LoginRequiredMixin, generic.CreateView):
   template_name = "leads/lead_create.html"
   form_class = LeadModelForm
 
@@ -46,7 +47,7 @@ class LeadCreateView(generic.CreateView):
     return super(LeadCreateView, self).form_valid(form)
 
 
-class LeadUpdateView(generic.UpdateView):
+class LeadUpdateView(LoginRequiredMixin, generic.UpdateView):
   template_name = "leads/lead_update.html"
   queryset = Lead.objects.all()
   form_class = LeadModelForm
@@ -55,7 +56,7 @@ class LeadUpdateView(generic.UpdateView):
     return reverse("leads:lead-detail")
 
 
-class LeadDeleteView(generic.DeleteView):
+class LeadDeleteView(LoginRequiredMixin, generic.DeleteView):
   template_name = "leads/lead_delete.html"
   queryset = Lead.objects.all()
 
